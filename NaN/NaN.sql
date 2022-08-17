4f1c8dfff10da717568c2cb0614879c3d892d5ca
Braian-Valor <69824471+Braian-Valor@users.noreply.github.com>
2022-08-16 21:25:12 -0300

    Add files via upload

---
+++ NaN/NaN.sql
@@ -0,0 +1,34 @@
+Create Database MuchosaMuchos
+Go
+Use MuchosaMuchos
+Go
+
+Create Table Anfitriones(
+	Legajo bigint not null Primary key,
+	Apellido varchar(30) not null,
+	Nombre varchar(30) not null,
+	FechaNacimiento date not null
+)
+Go
+
+Create Table JuegosPorAnfitrion(
+	Legajo bigint not null,
+	IdJuego int not null,
+	Estado varchar(20) not null
+)
+Go
+Alter Table JuegosPorAnfitrion
+Add Constraint PK_JuegosPorAnfitrion Primary key(Legajo, IdJuego)
+Go
+Alter Table JuegosPorAnfitrion
+Add Constraint FK_JuegosPorAnfitrion Foreign key(Legajo) references Anfitriones(Legajo)
+Go
+Alter Table JuegosPorAnfitrion
+Add Constraint FK2_JuegosPorAnfitrion Foreign key(IdJuego) references Juegos(Id)
+Go
+
+Create Table Juegos(
+	Id int not null Primary key,
+	Nombre varchar(30)
+)
+Go
